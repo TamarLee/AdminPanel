b42c080a6410126dabe4f2c518f579eb08601ef6
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2018-01-10 22:30:25 +0400

    ფორმის დამატება

---
+++ resources/views/admin/products/create.blade.php
@@ -0,0 +1,21 @@
+
+
+<form action="/admin/products/store" method="post">
+	<div>
+		<p>name</p>
+		<div>
+			<input type="text" name="name" id="name">
+		</div>
+	</div>
+
+	<div>
+		<p>description</p>
+		<div>
+			<textarea name="description" id="description"></textarea>
+			</div>
+	</div>
+	<br>
+	
+	<button>add products</button>
+	
+</form>

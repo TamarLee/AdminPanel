6ca8891f2863e64691d17dc63ee4494704ecf094
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2018-01-10 22:59:28 +0400

    add ფუნქციის დამატება

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -35,7 +35,7 @@ class ProductsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd ($request->all());
     }
 
     /**
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,21 +1,24 @@
 
 
-<form action="/admin/products/store" method="post">
+<form action="{{ url('/admin/products') }}" method="post">
+	{{ csrf_field() }}
 	<div>
 		<p>name</p>
 		<div>
 			<input type="text" name="name" id="name">
+			
 		</div>
 	</div>
 
 	<div>
 		<p>description</p>
 		<div>
-			<textarea name="description" id="description"></textarea>
+			<textarea name="description"></textarea>
+			
 			</div>
 	</div>
 	<br>
-	
+
 	<button>add products</button>
 	
 </form>
